f0aa4ffec171b07cda2fea532af1701a0e61932a
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-05-25 18:34:20 +0900

    add init

--- DependencyClass/CompoundDependency.py
+++ DependencyClass/CompoundDependency.py
@@ -2,5 +2,7 @@ import Dependency
 
 class CompoundDependency(Dependency):
     """description of class"""
+    def __init__(self, id,  name, lineno, kind, compound):
+        super.__init__(self, id,  name, lineno, kind, compound)
 
 
