3d27ce2d074a46f45ece868f0b8b349609fb22cb
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-06-10 16:48:28 +0900

    output dep

--- Doxydeps.py
+++ Doxydeps.py
@@ -2,29 +2,34 @@ import sys
 from GetDeps import GetDependencies
 from git import *
 import time
+import csv
+import os
 
 argv = sys.argv
 argc = len(argv)
 
-if argc == 2:
+if argc == 3:
     repopass = (argv[1]) # ファイルパス
+    csvpass = (argv[2])
 else:
     print("Usage: %s repopass" % argv[0])
     sys.exit()
-
-
-gd = GetDependencies.GetDependencies()
+    open
+writer = csv.writer(open(csvpass, "w", encoding="utf-8"), lineterminator="\n")
+depwriter = csv.writer(open("dep.csv", "w", encoding="utf-8"), lineterminator="\n")
+gd = GetDependencies.GetDependencies(depwriter)
 
 repo = Repo(repopass)
 
-for commit_no, item in enumerate(repo.iter_commits('master', max_count=10)):
+for commit_no, item in enumerate(repo.iter_commits('master')):
+    repo.git.checkout(item)
+    os.system("doxygen GetDeps/source/.config")
     d = time.gmtime(item.committed_date)
     date = ("%d-%d-%d" % (d.tm_year, d.tm_mon, d.tm_mday))
 
     for file in item.stats.files.keys():
         file_loc = gd.get_file_location(file)
         if file_loc != None:
-            print("%d, %s, %s, root" % (commit_no, file_loc, date))
-
+            writer.writerow((commit_no, file_loc, date, "root"))
             if len(item.parents) > 1:
                 gd.get_deps(file, commit_no, date)
--- GetDeps/GetDependencies.py
+++ GetDeps/GetDependencies.py
@@ -5,9 +5,10 @@ from GetDeps.DependencyClass import IndexDependency as IDp
 from GetDeps.Util import Util
  
 class GetDependencies(object):
-    def __init__(self):
+    def __init__(self, writer):
         # 依存関係のindexを生成
         self.__index = IDp.IndexDependency('index')
+        self.__writer = writer
 
     def file_to_depdict(self, root_dict:dict, is_to_dep:bool):
         dep_dict = {}
@@ -24,7 +25,8 @@ class GetDependencies(object):
 
     def output_dep(self, file_dict:dict, cono, date, state):
         for no, dp in enumerate(file_dict.values()):
-            print("%d, %s, %s, %s" % (cono, dp.get_location(), date, state))
+            #print("%d, %s, %s, %s" % (cono, dp.get_location(), date, state))
+            self.__writer.writerow((cono, dp.get_location(), date, state))
 
     def get_file_location(self, filepass):
         fileref = self.__index.get_file_ref(filepass)
@@ -46,16 +48,16 @@ class GetDependencies(object):
         cy = self.file_to_depdict({self.__fileref:self.__fdp}, True)
         self.output_dep(cy, cono, date, "to")
 
-        # from(依存されているものに)_from(依存されている)
-        ed2 = self.file_to_depdict(ed, False)
-        self.output_dep(ed2, cono, date, "from_from")
+        ## from(依存されているものに)_from(依存されている)
+        #ed2 = self.file_to_depdict(ed, False)
+        #self.output_dep(ed2, cono, date, "from_from")
 
-        # from(依存されているものに)_to(依存している)
-        ed2 = self.file_to_depdict(ed, True)
-        self.output_dep(ed2, cono, date, "from_to")
+        ## from(依存されているものに)_to(依存している)
+        #ed2 = self.file_to_depdict(ed, True)
+        #self.output_dep(ed2, cono, date, "from_to")
 
-        cy2 = self.file_to_depdict(cy, False)
-        self.output_dep(cy2, cono, date, "to_from")
+        #cy2 = self.file_to_depdict(cy, False)
+        #self.output_dep(cy2, cono, date, "to_from")
 
-        cy2 = self.file_to_depdict(cy, True)
-        self.output_dep(cy2, cono, date, "to_to")
+        #cy2 = self.file_to_depdict(cy, True)
+        #self.output_dep(cy2, cono, date, "to_to")
