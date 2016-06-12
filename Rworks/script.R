c4f6cee854b96f24e25252e08001852ac4f1a3c3
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-06-12 16:08:03 +0900

    fix

--- Rworks/script.R
+++ Rworks/script.R
@@ -5,9 +5,16 @@ roots <- read.csv("../root.csv", sep = ',', header = TRUE, row.names = NULL)
 deps <- read.csv("../dep.csv", sep = ',', header = TRUE, row.names = NULL)
 
 from <- subset(deps, deps$kind == "from")
+print(min(from$commitNo))
 
 for (dep in 1:nrow(from)) {
-    print(from$file_location[dep])
+    fc = from$commitno[dep]
+    fl = from$file_location[dep]
+    nextchange <- subset(roots, 
+                        (roots$commitno > fc) &&
+                         roots$file_location == fl, c(date, commitNo))
+    print(roots$commitNo)
+
     #for (root in 1:nrow(roots)) {
         
     #}
