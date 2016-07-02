f1cf7cecce6e1aa7573576426ed28f835b73626c
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-07-02 10:26:02 +0900

    improve for warnings

--- Rworks/Rworks/makeSub.R
+++ Rworks/Rworks/makeSub.R
@@ -1,5 +1,6 @@
 data_set <- read.csv("../../dep.csv", sep = ',', header = TRUE, row.names = NULL)
 data_set$file_location <- as.character(data_set$file_location)
+data_set$author <- as.character(data_set$author)
 data_set$date <- as.Date(data_set$date)
 
 roots <- subset(data_set, data_set$kind == "root")
@@ -10,21 +11,20 @@ deps$SubDate <- NA
 deps$same_author <- NA
 
 for (dep in 1:nrow(deps)) {
-    fc <- deps$commitNo[dep]
-    fl <- deps$file_location[dep]
-    fd <- deps$date[dep]
+    d <- deps[dep,]
+    nc <- subset(roots, roots$commitNo <= d$commitNo & roots$file_location == d$file_location)
+    #nc <- subset(nc, nc$file_location == d$file_location)
 
-    nc <- subset(roots, roots$commitNo <= fc)
-    nc <- subset(nc, nc$file_location == fl)
-    maxcommit <- max(nc$commitNo)
-    nc <- subset(nc, nc$commitNo == maxcommit)
 
     if (nrow(nc) > 0) {
-        #deps$SubDate[dep] <- min(nc$date) - fd
-        #deps$SubNo[dep] <- fc - max(nc$commitNo)
-        deps$SubDate[dep] <- nc$date[1] - fd
-        deps$SubNo[dep] <- fc - nc$commitNo[1]
-        deps$same_author <- (nc$author[1] == deps$author)
+        maxcommit <- max(nc$commitNo)
+        nc2 <- subset(nc, nc$commitNo == maxcommit)
+        n <- nc2[1,]
+        #deps$SubDate[dep] <- min(nc2$date) - fd
+        #deps$SubNo[dep] <- fc - max(nc2$commitNo)
+        deps$SubDate[dep] <- n$date - d$date
+        deps$SubNo[dep] <- d$commitNo - maxcommit
+        deps$same_author[dep] <- (n$author == d$author)
     }
 }
 deps <- na.omit(deps)
