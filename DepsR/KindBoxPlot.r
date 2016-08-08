71e0839a62d8f7f48a778372f95d016def102f09
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-08-08 19:44:41 +0900

    diviede kindplot

--- DepsR/KindBoxPlot.r
+++ DepsR/KindBoxPlot.r
@@ -6,8 +6,8 @@ kindnum <- c()
 deps$kind <- factor(deps$kind, levels = kindset)
 deps$file_location <- as.character(deps$file_location)
 
-dups <- deps[duplicated(data.frame(deps$commitNo, deps$file_location)),]
-deps <- deps[!duplicated(data.frame(deps$commitNo, deps$file_location)),]
+#dups <- deps[duplicated(data.frame(deps$commitNo, deps$file_location)),]
+#deps <- deps[!duplicated(data.frame(deps$commitNo, deps$file_location)),]
 
 #deps$date <- as.Date(deps$date)
 deps$SubDate <- as.numeric(deps$SubDate)
@@ -25,56 +25,10 @@ print(by(deps$SubDate, deps$kind, summary))
 #plot.new()
 #pdf("author_test.pdf")
 
-boxplot(deps$SubDate ~ kind, data = deps, ylim = c(0, 200), names = kindset)
-
-#for (judge in c(TRUE, FALSE)) {
-    #i = i + 1
-    ##deps2 <- deps
-    ##deps2 <- subset(deps, deps$same_author == judge)
-    #deps2 <- subset(deps, deps$is_merge == judge)
-    ##deps2 <- deps
-    ##print(by(deps, deps$kind, summary))
-
-    ##dates <- list()
-    ##nos <- list()
-    ##judgepar <- c()
-
-    ##for (ki in kindset) {
-        ##dep2 <- subset(deps2, deps2$kind == ki)
-        ##dates <- append(dates, list(dep2$SubDate))
-        ##nos <- append(nos, list(dep2$SubNo))
-        ##judgepar <- append(judgepar, c(nrow(dep2) / nrow(subset(deps, deps$kind == ki))))
-    ##}
-
-    ##dates <- append(dates, list(deps2$SubDate))
-    ##nos <- append(nos, list(deps2$SubNo))
-    ##judgepar <- append(judgepar, c(nrow(deps2) / nrow(deps)))
-
-    ##kindset <- append(kindset, c("all"))
-
-    ##pdf(paste(judge_name, "/date_", judge, ".pdf", sep = ""))
-    ##par(cex.axis = 2.0)
-    ##boxplot(dates, names = kindset, ylim = c(0, 200))
-    #if (judge == TRUE) {
-        #par(cex.axis = 2)
-        #boxplot(deps2$SubDate ~ kind, data = deps2, boxwex = 0.25, at = 1:5 - 0.2, col = cols[i], ylim = c(0, 200))
-    #} else {
-        #boxplot(deps2$SubDate ~ kind, data = deps2, boxwex = 0.25, at = 1:5 + 0.2, col = cols[i], ylim = c(0, 200), add = TRUE, xaxt = "n", yaxt = "n")
-    #}
-    ##print(t.test(dates[[1]], dates[[3]]))
-    ##print(t.test(dates[[3]], dates[[2]]))
-    ##print(t.test(dates[[2]], dates[[4]]))
-    ##print(t.test(dates[[4]], dates[[5]]))
-
-    ##pdf(paste(judge_name, "/CommitNo_", judge, ".pdf", sep = ""))
-    ##boxplot(nos, names = kindset, ylim = c(0, 300))
-
-    ##pdf(paste(judge_name, "/par_", judge, ".pdf", sep = ""))
-    ##barplot(judgepar, names = kindset, ylim = c(0, 1))
-#}
-
-#par(family = "Japan1GothicBBB")
-#legend("topleft", legend = c("マージとなるコミット　　　　　　　", 
-                             #"マージではないコミット　　　　　　"),fill = cols, cex = 1.5)
-##legend("topleft", legend = c("同じ開発者によるコミット　　　　　　",
-                             ##"異なる開発者によるコミット　　　　　"), fill = cols, cex = 1.5)
+#boxplot(deps$SubDate ~ kind, data = deps, ylim = c(0, 200))
+for (ki in kindset) {
+    deps2 <- subset(deps, deps$kind == ki)
+    dates <- append(dates, list(deps2$SubDate))
+    judgepar <- append(judgepar, c(nrow(dep2) / nrow(subset(deps, deps$kind == ki))))
+}
+plot(dates, names = kindset)
---
+++ DepsR/KindBoxPlot_judge.r
@@ -0,0 +1,55 @@
+deps <- read.csv("./newdep_2.csv", sep = ',', header = TRUE, row.names = NULL)
+
+kindset <- c("depender", "depender2", "depender3", "depender4", "dependee", "dependee2", "other")
+kindnum <- c()
+
+deps$kind <- factor(deps$kind, levels = kindset)
+deps$SubDate <- as.numeric(deps$SubDate)
+deps$is_merge <- as.logical(deps$is_merge)
+
+judge_name <- "author"
+judge_name <- "merge"
+
+cols <- c("#FF8888", "#8888FF")
+i <- 0
+plot.new()
+for (judge in c(TRUE, FALSE)) {
+    i <- i + 1
+    if (judge_name == "author") {
+        deps2 <- subset(deps, deps$same_author == judge)
+        legend_name <- c("同じ開発者によるコミット　　　　　　",
+                         "異なる開発者によるコミット　　　　　")
+    } else {
+        deps2 <- subset(deps, deps$is_merge == judge)
+        legend_name <- c("マージとなるコミット　　　　　　　","マージではないコミット　　　　　　")
+    }
+
+    par(cex.axis = 2.0)
+    if (judge == TRUE) {
+        # ジャッジに当てはまる割合
+        judgepar <- c()
+        for (ki in kindset) {
+            judgepar <- append(judgepar, c(nrow(subset(deps2, deps2$kind == ki)) / nrow(subset(deps, deps$kind == ki))))
+        }
+        par(cex.axis = 2)
+        boxplot(deps2$SubDate ~ kind, data = deps2, boxwex = 0.25, at = 1:7 - 0.2, col = cols[i], ylim = c(0, 200))
+    } else {
+        #plot(judgepar, ylim = c(0, 100), add = TRUE, xaxt = "n", yaxt = "n")
+        boxplot(deps2$SubDate ~ kind, data = deps2, boxwex = 0.25, at = 1:7 + 0.2, col = cols[i], ylim = c(0, 200), add = TRUE, xaxt = "n", yaxt = "n")
+    }
+
+    #2標本検定
+    #print(t.test(dates[[1]], dates[[3]]))
+    #print(t.test(dates[[3]], dates[[2]]))
+    #print(t.test(dates[[2]], dates[[4]]))
+    #print(t.test(dates[[4]], dates[[5]]))
+
+    #pdf(paste(judge_name, "/CommitNo_", judge, ".pdf", sep = ""))
+    #boxplot(nos, names = kindset, ylim = c(0, 300))
+
+    #pdf(paste(judge_name, "/par_", judge, ".pdf", sep = ""))
+    #barplot(judgepar, names = kindset, ylim = c(0, 1))
+}
+
+par(family = "Japan1GothicBBB")
+legend("topleft", legend = legend_name, fill = cols, cex = 1.5)
