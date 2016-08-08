b96894fa5a449aa1b7692f2962a967ada7b8cc50
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-08-08 10:07:51 +0900

    add depender 3 4

--- DepsR/KindBoxPlot.r
+++ DepsR/KindBoxPlot.r
@@ -1,34 +1,31 @@
-deps <- read.csv("./dep_3.csv", sep = ',', header = TRUE, row.names = NULL)
+deps <- read.csv("./newdep_2.csv", sep = ',', header = TRUE, row.names = NULL)
 
-kindset <- c("depender", "depender2", "dependee", "dependee2", "other")
+kindset <- c("depender", "depender2","depender3","depender4", "dependee", "dependee2", "other")
+kindnum <- c()
 
 deps$kind <- factor(deps$kind, levels = kindset)
-data_set$file_location <- as.character(data_set$file_location)
+deps$file_location <- as.character(deps$file_location)
 
 dups <- deps[duplicated(data.frame(deps$commitNo, deps$file_location)),]
 deps <- deps[!duplicated(data.frame(deps$commitNo, deps$file_location)),]
 
-for (dupNo in 1:length(dups)) {
-    deps <- subset(deps, deps$commitNo != dups[dupNo]$commitNo | deps$file_location != dups[dupNo]$file_location)
-}
-
 #deps$date <- as.Date(deps$date)
 deps$SubDate <- as.numeric(deps$SubDate)
 #deps$SubNo <- as.numeric(deps$SubNo)
-
+print(by(deps$SubDate, deps$kind, summary))
 #judge_name <- "author"
 #judge_name <- "."
 
-cols <- c("#FF8888", "#8888FF")
+#cols <- c("#FF8888", "#8888FF")
 #plot(0, 0, type = "n",
      #xlim = range(1:(ncol(y) * 3)), # x Ž²‚ðL‚ß‚É‚Æ‚é
      #ylim = range(y), xlab = "Type", ylab = "Weight",
      #axes = FALSE)
-i = 0
-plot.new()
+#i = 0
+#plot.new()
 #pdf("author_test.pdf")
 
-boxplot(deps$SubDate ~ kind, data = deps, ylim = c(0, 200))
+boxplot(deps$SubDate ~ kind, data = deps, ylim = c(0, 200), names = kindset)
 
 #for (judge in c(TRUE, FALSE)) {
     #i = i + 1
