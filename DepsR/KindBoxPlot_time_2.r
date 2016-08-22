f152ad85140b1bbfcb1bff946babf3aa99d5cdf6
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-08-22 18:54:26 +0900

    output time

--- DepsR/KindBoxPlot_time_2.r
+++ DepsR/KindBoxPlot_time_2.r
@@ -1,14 +1,21 @@
 deps <- read.csv("./newdep_2.csv", sep = ',', header = TRUE, row.names = NULL)
 
 kindset <- c("depender", "depender2", "depender3", "depender4", "dependee", "dependee2", "other")
-kindnum <- c()
+cols <- c("#FF8888", "#88FF88", "#8888FF", "#FFFF88", "#FF88FF", "#88FFFF", "#000000")
 
 deps$kind <- factor(deps$kind, levels = kindset)
 deps$date <- as.Date(deps$date)
 deps$SubDate <- as.numeric(deps$SubDate)
 
 library(xts)
-library(zoo)
-ts <- xts(deps$Subdate, deps$date)
-print(head(deps))
-#plot(apply.monthly(ts, mean), ylim = c(0, 200))
+i = 1
+plot.new()
+#for (ki in kindset) {
+    #dep2 <- subset(deps, deps$kind == ki)
+    #ts <- xts(dep2$SubDate, dep2$date)
+    #plot(apply.daily(ts, median), ylim = c(0, 200))
+    #par(new = T)
+    #i = i + 1
+#}
+ts <- xts(deps$SubDate, deps$date)
+plot(apply.daily(ts, median), ylim = c(0, 200))
