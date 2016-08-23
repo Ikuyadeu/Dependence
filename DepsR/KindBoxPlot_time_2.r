a3cbe3aaafa2ac63c9aef396d64ef6822f8a5228
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-08-23 13:55:30 +0900

    plot complete

--- DepsR/KindBoxPlot_time_2.r
+++ DepsR/KindBoxPlot_time_2.r
@@ -1,7 +1,17 @@
 deps <- read.csv("./newdep_2.csv", sep = ',', header = TRUE, row.names = NULL)
 
-kindset <- c("depender", "depender2", "depender3", "depender4", "dependee", "dependee2", "other")
-cols <- c("#FF8888", "#88FF88", "#8888FF", "#FFFF88", "#FF88FF", "#88FFFF", "#000000")
+# 7 colors
+#kindset <- c("depender", "depender2", "depender3", "depender4", "dependee", "dependee2", "other")
+#cols <- c("#FF8888", "#88FF88", "#8888FF", "#FFFF88", "#FF88FF", "#88FFFF", "#000000", "#888888")
+
+# 5 colors
+#kindset <- c("depender", "depender2", "dependee", "dependee2", "other")
+#cols <- c("#FF0000", "#00FF00", "#0000FF", "#00FFFF", "#FF00FF")
+
+# 3 colors
+kindset <- c("depender", "dependee", "other")
+cols <- c("#FF0000", "#00FF00", "#0000FF")
+
 
 deps$kind <- factor(deps$kind, levels = kindset)
 deps$date <- as.Date(deps$date)
@@ -10,12 +20,15 @@ deps$SubDate <- as.numeric(deps$SubDate)
 library(xts)
 i = 1
 plot.new()
-#for (ki in kindset) {
-    #dep2 <- subset(deps, deps$kind == ki)
-    #ts <- xts(dep2$SubDate, dep2$date)
-    #plot(apply.daily(ts, median), ylim = c(0, 200))
-    #par(new = T)
-    #i = i + 1
-#}
-ts <- xts(deps$SubDate, deps$date)
-plot(apply.daily(ts, median), ylim = c(0, 200))
+for (ki in kindset) {
+    dep2 <- subset(deps, deps$kind == ki)
+    ts <- apply.monthly(xts(dep2$SubDate, dep2$date), median)
+    if (ki == "depender") {
+        last_ts <- ts
+    } else {
+        last_ts <- na.locf(merge(last_ts, ts))
+    }
+}
+
+plot.zoo(last_ts, ylim = c(0, 100), col = cols, plot.type = "single")
+legend("topleft", legend = kindset, fill = cols, cex = 1.0)
