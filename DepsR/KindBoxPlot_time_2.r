56f9340b61ef1727fbea045da3763dbd8f8c5f86
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-08-24 13:22:48 +0900

    add 2 colors

--- DepsR/KindBoxPlot_time_2.r
+++ DepsR/KindBoxPlot_time_2.r
@@ -1,17 +1,20 @@
 deps <- read.csv("./newdep_2.csv", sep = ',', header = TRUE, row.names = NULL)
 
 # 7 colors
-#kindset <- c("depender", "depender2", "depender3", "depender4", "dependee", "dependee2", "other")
-#cols <- c("#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#000000", "#FFFFFF")
+kindset <- c("depender", "depender2", "depender3", "depender4", "dependee", "dependee2", "other")
+cols <- c("#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#000000", "#FFFFFF")
 
 # 5 colors
-#kindset <- c("depender", "depender2", "dependee", "dependee2", "other")
-#cols <- c("#FF0000", "#00FF00", "#0000FF", "#00FFFF", "#FF00FF")
+kindset <- c("depender", "depender2", "dependee", "dependee2", "other")
+cols <- c("#FF0000", "#00FF00", "#0000FF", "#00FFFF", "#FF00FF")
 
 # 3 colors
 kindset <- c("depender", "dependee", "other")
 cols <- c("#FF0000", "#00FF00", "#0000FF")
 
+# 2 colors
+kindset <- c("depender", "dependee")
+cols <- c("#FF0000", "#00FF00")
 
 deps$kind <- factor(deps$kind, levels = kindset)
 deps$date <- as.Date(deps$date)
@@ -30,5 +33,5 @@ for (ki in kindset) {
     }
 }
 
-plot.zoo(last_ts, ylim = c(0, 100), col = cols, plot.type = "single")
+plot.zoo(last_ts, ylim = c(0, 100), col = cols, plot.type = "single", ylab="next changed date" , xlab="date")
 legend("topleft", legend = kindset, fill = cols, cex = 1.0)
