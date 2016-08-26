5889c4c608165dad6ecc9c315ed1e55b8d78e9c6
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-08-26 16:57:30 +0900

    use quaterly

--- DepsR/KindBoxPlot_time_2.r
+++ DepsR/KindBoxPlot_time_2.r
@@ -1,8 +1,21 @@
 deps <- read.csv("./newdep_2.csv", sep = ',', header = TRUE, row.names = NULL)
 
+# times
+# 2016-6-24 3.3.0
+# 2015-12-15 3.2.0
+# 2015-10-7 3.1.0
+# 2015-6-24 3.0.0
+# 2014-12-18 3.0.0 dev
+# 2014-5-27 2.1
+# 2013-7-25 2.0.0
+# 2013-5-14 2.0.0 dev
+# 2012-5-9 1.0.0
+
+
 # 7 colors
 kindset <- c("depender", "depender2", "depender3", "depender4", "dependee", "dependee2", "other")
 cols <- c("#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#000000", "#FFFFFF")
+deps$kind <- factor(deps$kind, levels = kindset)
 
 # 5 colors
 kindset <- c("depender", "depender2", "dependee", "dependee2", "other")
@@ -12,26 +25,41 @@ cols <- c("#FF0000", "#00FF00", "#0000FF", "#00FFFF", "#FF00FF")
 kindset <- c("depender", "dependee", "other")
 cols <- c("#FF0000", "#00FF00", "#0000FF")
 
-# 2 colors
-kindset <- c("depender", "dependee")
-cols <- c("#FF0000", "#00FF00")
+## 2 colors
+#kindset <- c("other", "dependee")
+#cols <- c("#FF0000", "#00FF00")
 
-deps$kind <- factor(deps$kind, levels = kindset)
+## 2 colors
+#kindset <- c("depender", "other")
+#cols <- c("#FF0000", "#0000FF")
+
+## 2 colors
+#kindset <- c("depender", "dependee")
+#cols <- c("#FF0000", "#00FF00")
+
+# 2 colors
+#kindset <- c("dependee", "dependee2")
+#cols <- c("#FF0000", "#00FF00")
+
 deps$date <- as.Date(deps$date)
 deps$SubDate <- as.numeric(deps$SubDate)
+
+deps$file_location <- as.character(deps$file_location)
+deps <- deps[!duplicated(data.frame(deps$commitNo, deps$file_location)),]
 
 library(xts)
 i = 1
 plot.new()
 for (ki in kindset) {
     dep2 <- subset(deps, deps$kind == ki)
-    ts <- apply.monthly(xts(dep2$SubDate, dep2$date), median)
-    if (ki == "depender") {
+    ts <- apply.quarterly(xts(dep2$SubDate, dep2$date), median)
+    if (i == 1) {
         last_ts <- ts
+        i = 0
     } else {
         last_ts <- na.locf(merge(last_ts, ts))
     }
 }
 
-plot.zoo(last_ts, ylim = c(0, 100), col = cols, plot.type = "single", ylab="next changed date" , xlab="date")
+plot.zoo(last_ts, ylim = c(0, 100), col = cols, plot.type = "single", ylab = "next changed date", xlab = "date")
 legend("topleft", legend = kindset, fill = cols, cex = 1.0)
