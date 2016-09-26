cfa1fc9e3f967338947617beea1b92feb0433dbb
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-09-26 20:27:55 +0900

    add axis

--- DepsR/KindBoxPlot.r
+++ DepsR/KindBoxPlot.r
@@ -1,28 +1,58 @@
-deps <- read.csv("vert/deps.csv", sep = ',', header = TRUE, row.names = NULL)
+#deps <- read.csv("./newdep_2.csv", sep = ',', header = TRUE, row.names = NULL)
 
-kindset <- c("e", "ee", "r","rr","er","re", "o")
+## 7 colors
+#kindset <- c("e", "ee", "r", "rr", "o")
+## 5 colors
+#kindset <- c("depender", "depender2", "dependee", "dependee2", "other")
 
-deps$kind <- factor(deps$kind, levels = kindset)
-deps$file_location <- as.character(deps$file_location)
+## 3 colors
+##kindset <- c("r", "e", "o")
 
-#dups <- deps[duplicated(data.frame(deps$commitNo, deps$file_location)),]
-deps <- deps[!duplicated(data.frame(deps$commitNo, deps$file_location)),]
+### 2 colors
+##kindset <- c("o", "e")
 
+### 2 colors
+##kindset <- c("r", "o")
+
+### 2 colors
+##kindset <- c("r", "e")
+
+## 2 colors
+##kindset <- c("e", "ee")
+
+#deps$kind <- factor(deps$kind, levels = kindset)
 #deps$date <- as.Date(deps$date)
-deps$SubDate <- as.numeric(deps$SubDate)
-#deps$SubNo <- as.numeric(deps$SubNo)
-print(by(deps$SubDate, deps$kind, summary))
-#judge_name <- "author"
-#judge_name <- "."
+#deps$SubDate <- as.numeric(deps$SubDate)
 
-#cols <- c("#FF8888", "#8888FF")
-#plot(0, 0, type = "n",
-     #xlim = range(1:(ncol(y) * 3)), # x Ž²‚ðL‚ß‚É‚Æ‚é
-     #ylim = range(y), xlab = "Type", ylab = "Weight",
-     #axes = FALSE)
+#deps$file_location <- as.character(deps$file_location)
+#deps <- deps[!duplicated(data.frame(deps$commitNo, deps$file_location)),]
+plot.new()
+par(cex.axis = 2.5)
+#par(tck = 1)
+boxplot(deps$SubDate ~ kind, data = deps, 
+    ylim = c(0, 200), 
+    names = c("depender", "depender2", "dependee", "dependee2", "other"), 
+    xaxt = "n",
+    yaxt = "n",
+    lwd = 2.5,
+    yaxp = c(0, 200, 20))
+par(tck = NA)
+axis(side = 2,
+     tck = 1,
+     lty = 2,
+     lwd.ticks = 1,
+     yaxp = c(0, 200, 20)
+     )
+axis(side = 1,
+     at = 1:5, 
+     labels = c("depender", "depender2", "dependee", "dependee2", "other"))
+#print(by(deps, deps$kind, summary))
 #i = 0
-#plot.new()
-#pdf("author_test.pdf")
-
-#boxplot(deps$SubDate ~ kind, data = deps, ylim = c(0, 200))
-boxplot(deps$SubDate ~ kind, data = deps, ylim = c(0, 100))
+#for (ki in kindset) {
+    #d1 <- subset(deps, deps$kind == ki)$SubDate
+    #if (i != 0) {
+        #print(t.test(d1, d2, var.equal = F))
+    #}
+    #d2 <- d1
+    #i = 1
+#}
