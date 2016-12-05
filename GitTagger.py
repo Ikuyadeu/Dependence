5fccaa0e63d849fd36e65fee8b81620286a0a6c4
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2016-12-05 16:48:43 +0900

    make git to treetagger

---
+++ GitTagger.py
@@ -0,0 +1,34 @@
+import sys
+import csv
+from git import Repo
+import treetaggerwrapper
+
+ARGV = sys.argv
+ARGC = len(ARGV)
+
+if ARGC == 4:
+    REPO_PASS =  (ARGV[2]) + (ARGV[1]) # ファイルパス
+    CSV_PASS = (ARGV[1]) + "message.csv"
+    BRANCH_NAME = (ARGV[3])
+else:
+    print("Usage: %s repopass csvpass master_branch_name" % ARGV[0])
+    sys.exit()
+
+REPO = Repo(REPO_PASS)
+
+COMMITS_LEN = len(list(REPO.iter_commits(BRANCH_NAME)))
+
+
+WRITER = csv.writer(open(CSV_PASS, "w", encoding="utf-8"), lineterminator="\n")
+tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR='C:\TreeTagger')
+
+for commit_no, item in enumerate(REPO.iter_commits(BRANCH_NAME)):
+    
+    commitmessage = item.message.replace('\n','')
+
+
+    #tags = treetaggerwrapper.make_tags(tagger.tag_text(commitmessage), exclude_nottags=True)
+    #clemma = [x.lemma for x in tags]
+    #print(clemma)
+    WRITER.writerow((commit_no, commitmessage))
+
