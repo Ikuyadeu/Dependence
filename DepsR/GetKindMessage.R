572df088dfc5d7858ed445622f0dfdbfe1222859
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2017-01-17 11:54:48 +0900

    adopt some projects

--- DepsR/GetKindMessage.R
+++ DepsR/GetKindMessage.R
@@ -1,16 +1,22 @@
-project.name <- "vert.x"
-project.deps <- paste(project.name, "deps.csv", sep = "/")
-project.messages <- paste(project.name, "message.csv", sep = "/")
+projects <- c("egit", "egit-github", "vert.x")
 
-deps <- read.csv(project.deps, sep = ',', header = TRUE, row.names = NULL)
+for (i in 1:length(projects)) {
+    project.name <- projects[i]
 
-deps$futureNo <- deps$commitNo - deps$SubNo
+    project.deps <- paste(project.name, "deps.csv", sep = "/")
+    project.messages <- paste(project.name, "message.csv", sep = "/")
 
-messages <- read.csv(project.messages, sep = ',', header = TRUE, row.names = NULL)
-messages$message <-as.character(messages$message)
-kinds <- levels(deps$kind)
-for (kind2 in 1:length(kinds)) {
-    future <- subset(deps, deps$kind == kinds[kind2])$futureNo
-    messages$iskind <- messages$commitNo %in% future
-    write.csv(messages, paste(project.name, paste(kinds[kind2], "message.csv", sep = ""), sep = "/"), quote = TRUE, row.names = FALSE)
+    deps <- read.csv(project.deps, sep = ',', header = TRUE, row.names = NULL)
+
+    #deps$futureNo <- deps$commitNo - deps$SubNo
+
+    messages <- read.csv(project.messages, sep = ',', header = TRUE, row.names = NULL)
+    messages$message <- as.character(messages$message)
+    kinds <- levels(deps$kind)
+    for (kind2 in 1:length(kinds)) {
+        #future <- subset(deps, deps$kind == kinds[kind2])$futureNo
+        future <- subset(deps, deps$kind == kinds[kind2])$commitNo
+        messages$iskind <- messages$commitNo %in% future
+        write.csv(messages, paste(project.name, paste(kinds[kind2], "message_b.csv", sep = ""), sep = "/"), quote = TRUE, row.names = FALSE)
+    }
 }
