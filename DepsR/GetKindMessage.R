c4a0d0bc6cf30364f58d6d33b1f2557bcb84dd16
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2017-02-16 16:28:39 +0900

    fix

--- DepsR/GetKindMessage.R
+++ DepsR/GetKindMessage.R
@@ -2,21 +2,21 @@ projects <- c("egit", "egit-github", "vert.x")
 
 for (i in 1:length(projects)) {
     project.name <- projects[i]
-
+    print(project.name)
     project.deps <- paste(project.name, "deps.csv", sep = "/")
-    project.messages <- paste(project.name, "message.csv", sep = "/")
+    project.messages <- paste(project.name, "one_message.csv", sep = "/")
 
     deps <- read.csv(project.deps, sep = ',', header = TRUE, row.names = NULL)
 
-    #deps$futureNo <- deps$commitNo - deps$SubNo
+    deps$futureNo <- deps$commitNo - deps$SubNo
 
     messages <- read.csv(project.messages, sep = ',', header = TRUE, row.names = NULL)
     messages$message <- as.character(messages$message)
     kinds <- levels(deps$kind)
     for (kind2 in 1:length(kinds)) {
-        #future <- subset(deps, deps$kind == kinds[kind2])$futureNo
-        future <- subset(deps, deps$kind == kinds[kind2])$commitNo
+        future <- subset(deps, deps$kind == kinds[kind2])$futureNo
+        #future <- subset(deps, deps$kind == kinds[kind2])$commitNo
         messages$iskind <- messages$commitNo %in% future
-        write.csv(messages, paste(project.name, paste(kinds[kind2], "message_b.csv", sep = ""), sep = "/"), quote = TRUE, row.names = FALSE)
+        write.csv(messages, paste(project.name, paste(kinds[kind2], "one_message.csv", sep = ""), sep = "/"), quote = TRUE, row.names = FALSE)
     }
 }
--- Double_NaiveBayesian.py
+++ Double_NaiveBayesian.py
@@ -8,9 +8,7 @@ ARGV = sys.argv
 ARGC = len(ARGV)
 
 if ARGC == 4:
-    REPO_PASS =  (ARGV[2]) + (ARGV[1]) # ?t?@?C???p?X
     CSV_PASS = "DepsR\\" + (ARGV[1]) + "\\"
-    BRANCH_NAME = (ARGV[3])
 else:
     print("Usage: %s repopass csvpass master_branch_name" % ARGV[0])
     sys.exit()
@@ -19,17 +17,16 @@ else:
 tagger = treetaggerwrapper.TreeTagger(TAGLANG='en',TAGDIR='C:\TreeTagger')
 def getwords(doc):
     tags = [x.lemma for x in treetaggerwrapper.make_tags(tagger.tag_text(doc), exclude_nottags=True) if not re.search("[0-9a-f]{10,}|[^0-9A-Za-z]", x.lemma)]
-    #tags = [x.lemma for x in tags if not re.search("[0-9a-f]{10,}|[^0-9A-Za-z]", x.lemma)]
     return  tags[0] if len(tags) > 0 else list()
 
-csvname = CSV_PASS + "double_message.csv"
-csvname_w = CSV_PASS + "one_double_message.csv"
+csvname = CSV_PASS + "one_double_message.csv"
+csvname_w = CSV_PASS + "one_double_message3.csv"
 with open(csvname, encoding="utf-8") as csvfile:
     dep_writer = csv.writer(open(csvname_w, "w", encoding="utf-8"), lineterminator="\n")
     kind_reader = csv.DictReader(csvfile)
-    kind_message = [(x['message'], x['fmessage']) for x in kind_reader if x['message'] != x['fmessage']]
-
+    kind_message = [(x['message'], x['fmessage']) for x in kind_reader if x['futureNo'] != x['commitNo']]
+    print("getkindmessage")
     counter = Counter(kind_message)
     dep_writer.writerow(('before_word','after_word', 'score'))
     for word, cnt in counter.most_common():
-        dep_writer.writerow((getwords(word[0]), getwords(word[1]), cnt))
+        dep_writer.writerow((word[0], word[1], cnt))
